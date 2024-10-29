b5046643d478ff69d3efb240c69b83ff3d2f8f63
eddemesa <eddemesa24@gmail.com>
2024-10-28 17:47:04 -0700

    commit-10-28-24-3

--- PROGRAMS/Extract/ETL.py
+++ PROGRAMS/Extract/ETL.py
@@ -1,21 +1,10 @@
 import subprocess
 
-# # Run script2.py as a subprocess
-# result = subprocess.run(['python', 'script2.py'], capture_output=True, text=True)
-#
-# # Print the output of script2.py
-# print("Output of script2.py:")
-# print(result.stdout)
-#
-# # Print any error messages
-# if result.stderr:
-#     print("Errors:")
-#     print(result.stderr)
-
 
 class ETL:
 
-    def extract(self):
+    @staticmethod
+    def extract():
         # Code to extract data from a source
         result1 = subprocess.run([
             'python',
@@ -23,7 +12,11 @@ class ETL:
             ], capture_output=True, text=True)
         print("Ran succefully: "
               "Converting_Multiple_TXT_files_from_multiple_dir_to_CSV.py")
-        # print(result1)
+        print(result1)
+
+    # Code to transform the extracted data
+    @staticmethod
+    def transform():
 
         result1 = subprocess.run([
             'python',
@@ -31,7 +24,7 @@ class ETL:
             ], capture_output=True, text=True)
         print("Ran succefully: "
               "Extract_Unique_SSID.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -39,7 +32,7 @@ class ETL:
             ], capture_output=True, text=True)
         print("Ran succefully: "
               "Extracting_Unique_Capabilities.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -47,7 +40,7 @@ class ETL:
         ], capture_output=True, text=True)
         print("Ran succefully: "
               "Simplify_Unique_SSID_Capabilities.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -55,7 +48,7 @@ class ETL:
             ], capture_output=True, text=True)
         print("Ran succefully: "
               "Extracting_Average_Signal_Level.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -63,18 +56,17 @@ class ETL:
             ], capture_output=True, text=True)
         print("Ran succefully: "
               "Aggregate_Frequency_Usage.py")
-        # print(result1)
+        print(result1)
 
-
-        # Code to transform the extracted data
-
+    @staticmethod
+    def load():
         result1 = subprocess.run([
             'python',
             'Converting_Wifi_Data_CSV_to_MySQL.py'
         ], capture_output=True, text=True)
         print("Ran succefully: "
               "Converting_Wifi_Data_CSV_to_MySQL.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -82,7 +74,7 @@ class ETL:
         ], capture_output=True, text=True)
         print("Ran succefully: "
               "Converting_SSID_CSV_to_MySQL_Table.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -90,7 +82,7 @@ class ETL:
         ], capture_output=True, text=True)
         print("Ran succefully: "
               "Converting_Average_Signal_Level_to_MySQL.py")
-        # print(result1)
+        print(result1)
 
         result1 = subprocess.run([
             'python',
@@ -98,25 +90,15 @@ class ETL:
         ], capture_output=True, text=True)
         print("Ran succefully: "
               "Converting_Aggregate_Frequency_Usage_to_MySQl.py")
-        # print(result1)
-
-
-
-    # def transform(self, data):
-
-    #     # Code to transform the extracted data
-    #     pass
-    #
-    # def load(self, transformed_data):
-    #     # Code to load the transformed data into a target
-    #     pass
+        print(result1)
 
 
 if __name__ == "__main__":
     etl = ETL()
-    data = etl.extract()
-    # print("Ran succefully: "
-    #       "Converting_Multiple_TXT_files_from_multiple_dir_to_CSV.py")
-    # return
-    # transformed_data = etl.transform(data)
-    # etl.load(transformed_data)
+    print("Processing Extraction......")
+    etl.extract()
+    print("Processing Transformation......")
+    etl.transform()
+    print("Processing Load - Conversion to MySQL Database ......")
+    etl.load()
+
